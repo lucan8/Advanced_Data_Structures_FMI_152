ea94e4f6d05d3baed6d1cfdbee7dcc2daa30edf9
Lucian Dracea <lucian.dracea@veridion.com>
2024-04-30 16:12:41 +0300

    added splayTree

--- utils/Trees/Trees.py
+++ utils/Trees/Trees.py
@@ -1,7 +1,8 @@
 from abc import abstractmethod
 
+
 class Node:
-    def __init__(self, key : int, priority : int):
+    def __init__(self, key: int, priority: int = 0):
         self.key = key
         self.priority = priority
         self.left = None
@@ -12,17 +13,17 @@ class Tree:
     def __init__(self):
         self.root = None
 
-    #Returns the node with key = node(arg)
+    # Returns the node with key = node(arg)
     @abstractmethod
-    def search(self, node : int) -> Node:
+    def search(self, node: int) -> Node:
         ...
 
-    #Returns true if insertion was done, false otherwise
+    # Returns true if insertion was done, false otherwise
     @abstractmethod
-    def insert(self, node : int) -> bool:
+    def insert(self, node: int) -> bool:
         ...
 
-    #Returns true if removal was done, false otherwise
+    # Returns true if removal was done, false otherwise
     @abstractmethod
-    def remove(self, node : int) -> bool:
+    def remove(self, node: int) -> bool:
         ...
