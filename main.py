231a2b5d2c598efd39d4574b9c08cf9e5b8888d2
lucan8 <gamer.lucan72@gmail.com>
2024-05-02 12:59:01 +0300

    Added printInfo

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from utils.Trees.ScapegoatTree import ScapegoatTree
 from utils.Trees.SplayTree import SplayTree
 from utils.Tests import test_generator, test_handler
 from utils.Tests.test_generator import TestConfig
-from utils.functions.functions import blockInsert, blockRemove, blockSearch
 def main():
     test_configs_treaps = [
                             TestConfig(1_000_000, 1_000_000),
@@ -11,11 +10,11 @@ def main():
                             TestConfig(1_000_000, 1_000_000_000_000),
                             TestConfig(1_000_000, 1_000_000_000_000_000),
                             TestConfig(1_000_000, 1_000_000_000_000_000_000),
-                            TestConfig(10_000_000, 1_000_000),
-                            TestConfig(10_000_000, 1_000_000_000),
-                            TestConfig(10_000_000, 1_000_000_000_000),
-                            TestConfig(10_000_000, 1_000_000_000_000_000),
-                            #TestConfig(100_000_000, 10_000_000),
+                            #TestConfig(10_000_000, 1_000_000),
+                            #TestConfig(10_000_000, 1_000_000_000),
+                            #TestConfig(10_000_000, 1_000_000_000_000),
+                            #TestConfig(10_000_000, 1_000_000_000_000_000),
+                            #TestConfig(30_000_000, 10_000_000),
                             #TestConfig(100_000_000, 1_000_000_000),
                             ]
     
@@ -26,8 +25,7 @@ def main():
     treap_insert_path = "tests/random_generated/Treaps/Treaps_insert"
     treap_search_path = "tests/random_generated/Treaps/Treaps_search"
     treap_remove_path = "tests/random_generated/Treaps/Treaps_remove"
-
-    #TO DO: time the min,max as well
+    
     for test_i, test_s, test_r in zip(
                                     test_handler.read_file(treap_insert_path),
                                     test_handler.read_file(treap_search_path),
@@ -36,10 +34,11 @@ def main():
         #The number of priority possibilites = 10 * max_size
         print(f"Test {test_i.index}({test_i.length, test_i.max_value}):")
         treap = Treap(0, 10 * test_i.length)
-        blockInsert(treap, test_i.array)
-        blockSearch(treap, test_s.array)
-        blockRemove(treap, test_s.array)
 
+        treap.blockInsert(test_i.array)
+        treap.printTreapInfo()
+        treap.blockSearch(test_s.array)
+        treap.blockRemove(test_r.array)
 
 if __name__ == "__main__":
     main()
